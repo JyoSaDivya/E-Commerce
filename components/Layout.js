f7c1195cefbdbc7ca305531923c44964ae1d354c
jyoti <jyoti123medge@gmail.com>
2023-08-16 13:55:44 +0530

    styles-added

--- components/Layout.js
+++ components/Layout.js
@@ -2,20 +2,21 @@ import Head from 'next/head';
 import { AppBar, Toolbar, Typography, Container } from '@material-ui/core';
 import React from 'react';
 import { Children } from 'react';
-
+import useStyles from '../utils/styles';
 export default function Layout({ children }) {
+  const classes = useStyles();
   return (
     <div>
       <Head>
         <title>Next Amazon</title>
       </Head>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.navbar}>
         <Toolbar>
           <Typography>Amazon</Typography>
         </Toolbar>
       </AppBar>
-      <Container>{children}</Container>
-      <footer>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
         <Typography>All rights reserved @Amazon</Typography>
       </footer>
     </div>
